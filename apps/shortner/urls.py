a7fb4c147d64c4e04f26e5abc328fcc906d557bf
raufzer <a_zerkhef@estin.dz>
2024-08-12 17:14:12 +0100

    Adjusting the URL patterns

--- apps/shortner/urls.py
+++ apps/shortner/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from .views import ShortnerListAPIView,ShortnerCreateAPIView 
 
 
 urlpatterns = [
-
+   path('', ShortnerListAPIView.as_view(), name='all_links'),
+   path('create/', ShortnerCreateAPIView.as_view(), name='create_api'),
     
 ]
