ebd3dce63be35e1bdbccdd766eada97a4a0c97c9
raufzer <a_zerkhef@estin.dz>
2024-08-12 15:39:46 +0100

    reBuild

--- apps/shortner/serailizers.py
+++ apps/shortner/serailizers.py
@@ -1,11 +1,5 @@
 from rest_framework import serializers
-from .models import NanoLink
 
 
-class NanoLinkSerailzer(serializers.ModelSerializer):
-    nano_link = serializers.SerializerMethodField()
 
-    class Meta:
-        model = NanoLink
-        fields = ['original_link', 'nano_link',
-                  'acces_count', 'key', 'created']
+
--- apps/shortner/views.py
+++ apps/shortner/views.py
@@ -5,22 +5,3 @@ from .models import NanoLink
 from .serailizers import NanoLinkSerailzer
 
 
-class NanoLinkViewSet(viewsets.ModelViewSet):
-    queryset = NanoLink.objects.all()
-    serializer_class = NanoLinkSerailzer
-    
-    def create(self, request):
-        serializer = NanoLinkSerailzer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request, pk=None):
-        try:
-            nano_link = NanoLink.objects.get(pk=pk)
-        except NanoLink.DoesNotExist:
-            return Response({'error': 'Link not found'}, status=status.HTTP_404_NOT_FOUND)
-        nano_link.acces_count += 1
-        nano_link.save()
-        serialzer = NanoLinkSerailzer(nano_link)
-        return Response(serialzer.data, status=status.HTTP_200_OK)
